3d92d89a928e672fc549e0fe83cf2afb661bfd59
Mai Fathy <maifathy@gmail.com>
2021-06-07 14:29:36 +0200

    added repos reducer, to manage state

---
+++ src/reducers/repos.js
@@ -0,0 +1,13 @@
+import { RECEIVE_REPOS } from '../actions/repos.js'
+
+export default function repos(state={}, action){
+  switch (action.type){
+    case RECEIVE_REPOS:
+      return {
+        ...state,
+        ...action.repos
+      }
+    default:
+      return state
+  }
+}
