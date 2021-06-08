79b9c91399eb1b26152e9ac2abf28cfba6e312fc
Mai Fathy <maifathy@gmail.com>
2021-06-08 22:18:20 +0200

    added search by page

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import { getData } from './helpers.js'
 import { gitHubUrl } from './_data.js'
 
-export function getInitialData () {
-  return getData(gitHubUrl)
+export function getInitialData (page) {
+  return getData(`${gitHubUrl}&page=${page}`)
   .then((repos) => ({
     repos
   }))
