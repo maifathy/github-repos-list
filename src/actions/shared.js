fadd5f80193ceef203df37c3e4810f9a1ca9206d
Mai Fathy <maifathy@gmail.com>
2021-06-08 22:15:13 +0200

    added search by page

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,10 +2,10 @@ import { getInitialData } from '../utils/api.js'
 import { receiveRepos } from '../actions/repos.js'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
-export default function handleInitialData(){
+export default function handleInitialData(page){
   return (dispatch) => {
     dispatch(showLoading())
-    return getInitialData()
+    return getInitialData(page)
     .then(({ repos }) => {
       dispatch(receiveRepos(repos))
       dispatch(hideLoading())
