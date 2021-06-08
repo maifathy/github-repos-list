36059632f2f21f6cf0a64e38dddedf77dc774dd5
Mai Fathy <maifathy@gmail.com>
2021-06-08 22:17:55 +0200

    added getDays method

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,16 @@
 export const getData = async (url = '') => {
-    const response = await fetch(url);
+  const response = await fetch(url);
 
-    try {
-        const data = await response.json();
-        return data;
-    } catch (e) {
-        console.log(`GET method error: ${e}`);
-    }
+  try {
+      const data = await response.json();
+      return data;
+  } catch (e) {
+      console.log(`GET method error: ${e}`);
+  }
+}
+
+export const getDays = (date) => {
+  const now = new Date()
+  const diff = now.getTime() - (new Date(date)).getTime()
+  return parseInt(diff / (1000*60*60*24))
 }
